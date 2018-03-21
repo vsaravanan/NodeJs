c63fadf57b0c3a5a6ddacd1efbbb37260d45f917
V Saravanan <saravanan.resume@gmail.com>
2018-03-21 14:01:38 +0800

    wines

--- NodejsWebApp/server.js
+++ NodejsWebApp/server.js
@@ -25,59 +25,15 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var router = express.Router();
-var indexRouter = express.Router();
-
-var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/mydemodb');
-
-//var db = mongoose.Schema({
-//    name: String,
-//    servingSize: String,
-//    calories: Number,
-//    gramsCarbo: Number,
-//    gramsProtein: Number,
-//    gramsFat: Number
-//});
-
-//var Food = mongoose.model('Food', foodSchema);
-
-var port = process.env.PORT || 1337;
-
-router.route('/foods')
-.post(function (req, res){
-    // create and store a new food document based on passed in data
-})
-.get(function (req, res){
-    // return a list of food documents from collection
+app.get('/wines', function (req, res) {
+    res.send([{ name: 'wine1' }, { name: 'wine2' }]);
+});
+app.get('/wines/:id', function (req, res) {
+    res.send({ id: req.params.id, name: "The Name", description: "description" });
 });
 
-router.route('/food/:id')
-.post(function (req, res){
-    //
-})
-.get(function (req, res){
-    // 
-})
-.put(function (req, res){
-    // 
-})
-.delete(function (req, res){
-    // 
-});
-
-indexRouter.get("/*", function (req, res) {
-    res.end("home single page app");
-})
 
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ 'extended': 'true' }));
-app.use(bodyParser.json());
+app.listen(3000);
+console.log('Listening on port 3000...');
 
-app.use('/', indexRouter);
-app.use('/api', router);
-
-app.listen(port, function () {
-    console.log("listening " + port);
-});
