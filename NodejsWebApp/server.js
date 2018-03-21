177829e4450b3549fb476c807d15cc28fffc25b7
V Saravanan <saravanan.resume@gmail.com>
2018-03-21 14:16:13 +0800

    added wines.js

--- NodejsWebApp/server.js
+++ NodejsWebApp/server.js
@@ -23,15 +23,13 @@ var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+var wines = require('./routes/wines');
+
 var app = express();
 
-app.get('/wines', function (req, res) {
-    res.send([{ name: 'wine1' }, { name: 'wine2' }]);
-});
-app.get('/wines/:id', function (req, res) {
-    res.send({ id: req.params.id, name: "The Name", description: "description" });
-});
 
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById);
 
 
 app.listen(3000);
